711e7084d47187a90625c052ce919ee6f38860f1
qazi <qazi.naveed@nextgeni.com>
2025-01-01 15:11:35 +0100

    first commit with default project

---
+++ CloneWeatherSDKiOS/CloneWeatherSDKiOS/CloneWeatherSDKiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneWeatherSDKiOSApp.swift
+//  CloneWeatherSDKiOS
+//
+//  Created by Qazi on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CloneWeatherSDKiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloneWeatherSDKiOS/CloneWeatherSDKiOSTests/CloneWeatherSDKiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloneWeatherSDKiOSTests.swift
+//  CloneWeatherSDKiOSTests
+//
+//  Created by Qazi on 01/01/2025.
+//
+
+import Testing
+@testable import CloneWeatherSDKiOS
+
+struct CloneWeatherSDKiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
